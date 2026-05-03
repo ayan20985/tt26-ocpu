c455395162fce0d4b80e385e181e4d85d1858253
ayan20985 <ayanali20985@gmail.com>
2026-05-03 13:08:35 -0400

    ocpu

--- test/tb.v
+++ test/tb.v
@@ -28,7 +28,7 @@ module tb ();
 `endif
 
   // Replace tt_um_example with your module name:
-  tt_um_vgaTestTest user_project (
+  tt_um_ocpu user_project (
 
       // Include power ports for the Gate Level test:
 `ifdef GL_TEST
